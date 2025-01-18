6e1b20d703fc63e56b30ef6d8965e43acab32881
Soham Gadekar <sohamg.3456@gmail.com>
2025-01-18 09:03:42 +0530

    added javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('nav ul');
+
+burger.addEventListener('click', function(){
+    navLinks.classList.toggle('active');
+});
+
+
